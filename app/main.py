de81295eaaeadc80bb6e5e1fd36532336bab7b41
Radoslaw <r.cybulski1122012@gmail.com>
2021-05-25 15:45:47 +0200

    Move from threading to selectors

--- app/main.py
+++ app/main.py
@@ -1,24 +1,10 @@
-import threading
-
 from server import Server
 from http_handling import HttpRequest, InvalidHttpRequest
 
 
-server = Server(host="127.0.0.1", port=8000)
+def fake_handler(request):
+    return b"HTTP/1.1 200 OK\n\nHello world\n"
 
 
-def handle_client(conn, addr):
-    with conn:
-        request_string = server.receive_request(conn)
-        try:
-            request = HttpRequest(request_string)
-        except InvalidHttpRequest:
-            response = f"HTTP/1.1 400 Bad Request"
-        else:
-            response = f"HTTP/1.1 200 OK\n\n{request.request_string}"
-        server.send_response(conn, response)
-
-
-while True:
-    connection, address = server.listen_for_client()
-    threading.Thread(target=handle_client, args=(connection, address)).start()
+server = Server(host="127.0.0.1", port=8000, request_handler=fake_handler)
+server.run()
--- app/server.py
+++ app/server.py
@@ -1,13 +1,19 @@
 import socket
+import selectors
+from types import SimpleNamespace
 
 
 class Server:
-    def __init__(self, host, port, *, encoding="utf-8"):
+    def __init__(self, host, port, request_handler, *, encoding="utf-8"):
         self.host = host
         self.port = port
         self.encoding = encoding
+        self.sel = selectors.DefaultSelector()
+        self.request_handler = request_handler
 
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.setblocking(False)
+        self.sel.register(self.s, selectors.EVENT_READ, data=None)
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.s.bind((host, port))
         self.s.listen()
@@ -15,13 +21,56 @@ class Server:
     def __del__(self):
         self.s.close()
 
-    def listen_for_client(self):
-        conn, addr = self.s.accept()
-        return conn, addr
+    def run(self):
+        while True:
+            events = self.sel.select(timeout=None)
+            for key, mask in events:
+                if key.data is None:
+                    self.handle_new_client(key)
+                else:
+                    self.service_connection(key, mask)
 
-    def receive_request(self, conn):
-        request = conn.recv(8192)
-        return request.decode(self.encoding)
+    def handle_new_client(self, key):
+        conn, addr = key.fileobj.accept()
+        conn.setblocking(False)
+        data = SimpleNamespace(addr=addr, request=b"", response=b"")
+        self.sel.register(conn, selectors.EVENT_READ, data=data)
 
-    def send_response(self, conn, response):
-        conn.sendall(response.encode(self.encoding))
+    def service_connection(self, key, mask):
+        conn = key.fileobj
+        data = key.data
+
+        if mask & selectors.EVENT_READ:
+            recv_data = conn.recv(4096)
+            data.request += recv_data
+            if self._is_request_fully_received(data.request) or not recv_data:
+                data.response = self.request_handler(data.request)
+                self.sel.modify(conn, selectors.EVENT_WRITE, data=data)
+
+        if mask & selectors.EVENT_WRITE:
+            if data.response:
+                sent = conn.send(data.response)
+                data.response = data.response[sent:]
+            else:
+                self.sel.unregister(conn)
+                conn.close()
+
+    def _is_request_fully_received(self, request):
+        request = request.decode(self.encoding)
+        request = request.replace("\r", "")
+        if "\n\n" not in request:
+            return False
+        else:
+            request_and_headers, body = request.split("\n\n")
+            for line in request_and_headers.split("\n"):
+                if "content-length" in line.lower():
+                    content_length = int(line.split(":")[1].strip())
+                    break
+            else:
+                return True
+
+            content = request.split("\n\n", 1)[1]
+            if len(content) == content_length:
+                return True
+            else:
+                return False
