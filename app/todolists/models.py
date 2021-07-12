737c0c031ab459fac644dd31467c57964f28d5df
Radoslaw <r.cybulski1122012@gmail.com>
2021-07-12 19:05:42 +0200

    Add 'caching' to todolists models properties

--- app/todolists/models.py
+++ app/todolists/models.py
@@ -8,16 +8,25 @@ class ToDoList(Model):
     description = Field("varchar(256)", default="''", validators=[MaxLenValidator(256)])
     creator_id = Field("integer")
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._creator = None
+        self._tasks = None
+
     @property
     def creator(self):
-        try:
-            return AppUser.select(id_=self.creator_id)[0]
-        except IndexError:
-            return None
+        if self._creator is None:
+            try:
+                self._creator = AppUser.select(id_=self.creator_id)[0]
+            except IndexError:
+                return None
+        return self._creator
 
     @property
     def tasks(self):
-        return Task.select(todolist_id=self.id_)
+        if self._tasks is None:
+            self._tasks = Task.select(todolist_id=self.id_)
+        return self._tasks
 
 
 class Task(Model):
@@ -27,9 +36,16 @@ class Task(Model):
     is_done = Field("boolean", default=False)
     todolist_id = Field("integer")
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._todolist = None
+
     @property
     def todolist(self):
-        try:
-            return ToDoList.select(id_=self.todolist_id)[0]
-        except IndexError:
-            return None
+        if self._todolist is None:
+            try:
+                self._todolist = ToDoList.select(id_=self.todolist_id)[0]
+            except IndexError:
+                return None
+
+        return self._todolist
